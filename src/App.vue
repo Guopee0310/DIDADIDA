9d5f0d59a5fb0f589088690ca3a17443e160b00e
Guopee0310 <Guopee0310@gmail.com>
2023-09-04 15:10:22 +0800

    upload-1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
+
+<PageHeader :breadcrumb-list="breadcrumbList">
+        <template #title><div class="demo-title">Title</div></template>
+        <template #logo><div class="demo-logo"><img src="~@/assets/images/dida_logo.png" alt=""></div></template>
+        <template #content><div class="demo-content">Content</div></template>
+        <template #extra><div class="demo-extra">Extra</div></template>
+        <template #action><div class="demo-action">Action</div></template>
+</PageHeader>
+
+
+
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+
+  <Button type="success" ghost>Success</Button>
+  <div class="colorcolor">123</div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+// @import "~@/assets/scss/base/variables.scss";
+//區域使用範例
+.demo-logo{
+  width: 50px;
+  img{
+    width: 100%;
+  }
+}
+.colorcolor{
+  color:$privecolor;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,3 +53,17 @@ nav {
   }
 }
 </style>
+
+<script>
+    export default {
+        data () {
+            return {
+                breadcrumbList: [
+                    {
+                        title: '面包屑'
+                    }
+                ]
+            }
+        }
+    }
+</script>
