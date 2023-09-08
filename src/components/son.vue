ff46ef57cc17514d00c853315d9e380a9cfeeb67
jason <abc@gmail.com>
2023-09-08 21:14:03 +0800

    product cards排版

--- src/components/son.vue
+++ src/components/son.vue
@@ -1,12 +1,33 @@
 <template>
-    
-    
-    <div class="product_card">
-        <heart></heart>
-        <img src="../assets/logo.png" alt="">
-        <span>NTD450</span>
-         <p>Lorem ipsum dolor sit</p>
+  <div class="card">
+    <div class="heart">
+      <heart></heart>
     </div>
+    <div class="pic">
+      <a href="#"><img src="../assets/images/dolphin_pillow.jpg" alt=""></a>
+    </div>
+
+    <div class="name">
+      <a href=""> 大型乾燥花束【和煦】</a>
+    </div>
+    <div class="info">
+      <div class="cost">
+        <span>NT 750</span>
+      </div>
+      <div class="prod_btn">
+        <div class="num">
+          <input type="button" value="–">
+          <input type="text" value="1" size="1">
+          <input type="button" value="+">
+        </div>
+        <div class="buy">
+          <a href="">
+            <i class="fa-solid fa-cart-shopping" style="color: #9fbdce;"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
   
 <script>
@@ -15,21 +36,137 @@ import heart from './heart.vue'
 
 export default {
   name: 'son',
-  props:{
-    msg1:[String,Number]
+  props: {
+    msg1: [String, Number]
   },
-  components:{
+  components: {
     btn2,
     heart
   }
 }
 </script>
-<style scoped>
-  .product_card{
-    width: calc(100% / 4 - 20px);
-    border: 1px solid blue;
-    color: blue;
-    font-size: 20px;
-    margin-top: 20px;
+<style scoped lang="scss">
+.card {
+  width: 270px;
+  margin: 15px 15px 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  box-sizing: border-box;
+
+  .heart {
+    margin: 0 0 -50px auto;
+    padding: 0 5px;
+    z-index: 2;
   }
+
+  .pic {
+    overflow: hidden;
+    margin-bottom: 10px;
+  }
+
+  .pic img {
+    width: 100%;
+    vertical-align: top;
+    transform: scale(1);
+    transition: .7s;
+  }
+
+  .pic img:hover {
+    transition: .7s;
+    transform: scale(1.1);
+  }
+
+  .name {
+    font-size: 17px;
+    margin-bottom: 15px;
+    line-height: 1.5;
+    font-family: Gowun Batang, 'Times New Roman', Times, NotoSerifCJKtc-Regular, serif;
+    color: #333;
+  }
+
+  .name a {
+    text-decoration: none;
+    color: #333;
+    transition: .3s;
+    font-size: Gowun Batang, 'Times New Roman', Times, NotoSerifCJKtc-Regular, serif;
+    display: block;
+  }
+
+  span {
+    display: block;
+    // font-size: 17px;
+  }
+
+  .info {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .prod_btn{
+    display: flex;
+  }
+  .num input {
+    width: 30px;
+    border: 0;
+    text-align: center;
+    background-color: #9fbdce;
+    font-size: 15px;
+  }
+
+  .num input:first-child {
+    border-radius: 15px 0 0 15px;
+  }
+
+  .num input:last-child {
+    border-radius: 0 15px 15px 0;
+  }
+
+  .buy {
+    width: 35px;
+    height: 35px;
+    // border-radius: 100px;
+    // border: 1.5px solid #d9d9d9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .buy i {
+    font-size: 17px;
+  }
+
+  .buy .fa-cart-shopping {
+    width: 12px;
+    margin: 4px 9px 0 0;
+  }
+
+  .buy:hover .fa-plus {
+    display: none;
+  }
+
+  .buy:hover .fa-cart-shopping {
+    display: inline-block;
+  }
+
+  .buy:hover {
+    background-color: #eee;
+  }
+
+
+  .name a:hover {
+    color: #C48710;
+    transition: .2s;
+  }
+
+
+  .buy img:nth-child(1):hover {
+    transition: .2s;
+    opacity: 0;
+  }
+
+
+
+}
 </style>
--- src/views/product.vue
+++ src/views/product.vue
@@ -2,55 +2,59 @@
   <header1></header1>
   <wave></wave>
   <login></login>
-  
-<div class="product_allCards">
-  <Son :msg1="son" v-for="i in 20" ></Son> 
-</div>
+
+  <div class="product_allCards">
+    <Son :msg1="son" v-for="i in 20"></Son>
+  </div>
   <!-- card -->
   <footer1></footer1>
- </template>
+</template>
    
- <script>
- import Son from '../components/son.vue'
- import login from '../components/login.vue'
- import footer1 from '../components/footer.vue'
- import header1 from '../components/header.vue'
- import wave from '../components/wave.vue'
- export default {
-   name: 'product',
-   data() {
+<script>
+import Son from '../components/son.vue'
+import login from '../components/login.vue'
+import footer1 from '../components/footer.vue'
+import header1 from '../components/header.vue'
+import wave from '../components/wave.vue'
+export default {
+  name: 'product',
+  data() {
     return {
-        son:'子元件'
+      son: '子元件'
     }
-   },
-   props: {
-     msg: String
-   },
-   components:{
+  },
+  props: {
+    msg: String
+  },
+  components: {
     Son,
     footer1,
     login,
     header1,
     wave
-   }
- }
- </script>
-<style scoped lang="scss">
-@font-face {
-    font-family: 'YourFontFamily'; /* 替换为你的字体名称 */
-    src: url('../assets/font/NotoSerifCJKtc-Regular.otf') format('otf'); /* 替换为字体文件的实际路径 */
-    
   }
-  *{
-    font-family: 'YourFontFamily' 
-  }
-.product_allCards{
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-wrap: wrap;
 }
-div{
+</script>
+<style scoped lang="scss">
+// @font-face {
+//     font-family: 'YourFontFamily'; /* 替换为你的字体名称 */
+//     src: url('../assets/font/NotoSerifCJKtc-Regular.otf') format('otf'); /* 替换为字体文件的实际路径 */
+
+//   }
+* {
+  font-family: 'YourFontFamily'
+}
+
+.product_allCards {
+  width: 1200px;
+  margin: 50px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+div {
   font-size: map-get($fontSizes, 'div');
 }
 </style>
