c1ec6a060b3330b669d927ef82b5196db7248dde
a0956154555 <wayne870726@yahoo.com.tw>
2023-09-10 17:55:06 +0800

    banner H1切換 開關會員登錄

--- src/components/wave.vue
+++ src/components/wave.vue
@@ -1,68 +1,107 @@
 <template>
-    
-<div class="banner_pic">  
-<div class="bannerTitle">
-  <h1>周邊商品</h1>
-  <h2>SHOP</h2>
-</div>
-<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-  <defs>
-    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v30h-355z">
-    </path>
-  </defs>
-  <g class="parallax">
-    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.6" />
-    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(70, 130, 191,0.7)" />
-    <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
-  </g>
-</svg>
-</div>
+  <div class="banner_pic">
+    <div class="bannerTitle">
+      <h1>{{ h1Name }}</h1>
+      <h2>{{ h2Name }}</h2>
+    </div>
+    <svg
+      class="waves"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28"
+      preserveAspectRatio="none"
+      shape-rendering="auto"
+    >
+      <defs>
+        <path
+          id="gentle-wave"
+          d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v30h-355z"
+        ></path>
+      </defs>
+      <g class="parallax">
+        <use
+          xlink:href="#gentle-wave"
+          x="48"
+          y="0"
+          fill="rgba(255,255,255,0.6"
+        />
+        <use
+          xlink:href="#gentle-wave"
+          x="48"
+          y="3"
+          fill="rgba(70, 130, 191,0.7)"
+        />
+        <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+      </g>
+    </svg>
+  </div>
 </template>
-  
+
 <script>
-
-
 export default {
-  name: 'son',
- 
-}
+  name: "son",
+  data() {
+    return {
+      h1Title: "",
+      h2Title: "",
+    };
+  },
+  computed: {
+    h1Name() {
+      return this.$route.path == "/product"
+        ? "周邊商品"
+        : this.$route.path == "/explore"
+        ? "探索海洋生物"
+        : this.$route.path == "/news"
+        ? "最新消息"
+        : this.$route.path == "/about"
+        ? "關於我們"
+        : "";
+    },
+    h2Name() {
+      return this.$route.path == "/product"
+        ? "Shop"
+        : this.$route.path == "/explore"
+        ? "Animal"
+        : this.$route.path == "/news"
+        ? "News"
+        : this.$route.path == "/about"
+        ? "AboutUs"
+        : "";
+    },
+  },
+};
 </script>
 <style scoped lang="scss">
-  
 body {
-  margin:0;
+  margin: 0;
 }
 
+.banner_pic {
+  height: 90vh;
+  position: relative;
+  background-image: url(../../public/all_images/cenote-280252_1920.jpg);
+  background-position: bottom;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .bannerTitle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: map-get($colors, "light");
 
-.banner_pic{
-height:90vh;
-position: relative;
-background-image: url(../../public/all_images/cenote-280252_1920.jpg);
-background-position: bottom;
-background-size: cover;
-display: flex;
-justify-content: center;
-align-items: center;
-.bannerTitle{
- display: flex;
- justify-content: center;
- align-items: center;
- flex-direction: column;
- color: map-get($colors, 'light');
- 
- h1{
-  border-bottom: 1px solid map-get($colors, 'light');
- }
-  font-size: map-get($fontSizes, 'h1');
-  h2{
-    font-size: map-get($fontSizes, 'h2');
+    h1 {
+      border-bottom: 1px solid map-get($colors, "light");
+    }
+    font-size: map-get($fontSizes, "h1");
+    h2 {
+      font-size: map-get($fontSizes, "h2");
+    }
   }
 }
-}
-
-
-
 
 .waves {
   position: absolute;
@@ -72,11 +111,10 @@ align-items: center;
   vertical-align: top;
 }
 
-
 /* Animation */
 
 .parallax > use {
-  animation: move-forever 20s cubic-bezier(.55,.6,.59,.5)infinite;
+  animation: move-forever 20s cubic-bezier(0.55, 0.6, 0.59, 0.5) infinite;
 }
 .parallax > use:nth-child(1) {
   animation-delay: -2s;
@@ -93,16 +131,16 @@ align-items: center;
 
 @keyframes move-forever {
   0% {
-   transform: translate(-90px,0);
+    transform: translate(-90px, 0);
   }
-  100% { 
-    transform: translate(85px,0);
+  100% {
+    transform: translate(85px, 0);
   }
 }
 @media (max-width: 768px) {
-    .waves {
-        height: 40px;
-        min-height: 40px;
-    }
+  .waves {
+    height: 40px;
+    min-height: 40px;
+  }
 }
 </style>
