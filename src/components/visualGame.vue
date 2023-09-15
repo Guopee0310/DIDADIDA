8e1b9239c21e4b4e6bc19aa09fee9178eda96d6e
CHEN-HAO-YUAN <sandy87120509@gmail.com>
2023-09-16 01:10:08 +0800

    visualGame style問題

--- src/components/visualGame.vue
+++ src/components/visualGame.vue
@@ -15,19 +15,9 @@
 </template> -->
 <template>
   <div class="wrapper">
-    <video
-      :src="require('../assets/video/pexels_videos_2556894 (1080p).mp4')"
-      class="videoShark"
-      autoplay
-      loop
-      v-if="autoPlayTrue"
-      :key="aaa"
-      muted
-    ></video>
-    <div
-      class="shadow"
-      ref="shadow"
-    ></div>
+    <video :src="require('../assets/video/pexels_videos_2556894 (1080p).mp4')" class="videoShark" autoplay loop
+      v-if="autoPlayTrue" :key="aaa" muted></video>
+    <div class="shadow" ref="shadow"></div>
     <!-- 把shadow綁的style拿掉，解決遮蓋問題
       :style="{ transform: `translate(${x}px, ${y}px)` }" -->
   </div>
@@ -52,7 +42,18 @@ export default {
       this.x = x;
       this.y = y;
 
+      if (this.$refs.shadow) {
         this.$refs.shadow.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+      } else {
+        console.error("$refs.shadow is null");
+      }
+
+
+
+
+
+
+
     },
     changeBkPosition() {
       let x = e.clientX - document.documentElement.clientWidth * 1.5;
@@ -100,11 +101,9 @@ body {
   left: 50%;
   top: 50%;
   transform: translateX(-50%) translateY(-50%);
-  background-image: radial-gradient(
-    circle at center,
-    transparent,
-    #000000bf 10%
-  );
+  background-image: radial-gradient(circle at center,
+      transparent,
+      #000000bf 10%);
 }
 </style>
 
