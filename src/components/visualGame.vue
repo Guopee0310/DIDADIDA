819ef6fd112afa513c64d1aced55e0e4d8cf649e
a0956154555 <wayne870726@yahoo.com.tw>
2023-09-25 09:08:03 +0800

    改遊戲 shadow的vw

--- src/components/visualGame.vue
+++ src/components/visualGame.vue
@@ -15,8 +15,15 @@
 </template> -->
 <template>
   <div class="wrapper">
-    <video :src="require('../assets/video/pexels_videos_2556894 (1080p).mp4')" class="videoShark" autoplay loop
-      v-if="autoPlayTrue" :key="aaa" muted></video>
+    <video
+      :src="require('../assets/video/pexels_videos_2556894 (1080p).mp4')"
+      class="videoShark"
+      autoplay
+      loop
+      v-if="autoPlayTrue"
+      :key="aaa"
+      muted
+    ></video>
     <div class="shadow" ref="shadow"></div>
     <!-- 把shadow綁的style拿掉，解決遮蓋問題
       :style="{ transform: `translate(${x}px, ${y}px)` }" -->
@@ -43,17 +50,11 @@ export default {
       this.y = y;
 
       if (this.$refs.shadow) {
-        this.$refs.shadow.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+        this.$refs.shadow.style.transform =
+          "translate(" + x + "px, " + y + "px)";
       } else {
         console.error("$refs.shadow is null");
       }
-
-
-
-
-
-
-
     },
     changeBkPosition() {
       let x = e.clientX - document.documentElement.clientWidth * 1.5;
@@ -97,14 +98,16 @@ body {
 
 .shadow {
   position: absolute;
-  width: 250vw;
+  width: 500vw;
   height: 200vh;
   left: 50%;
   top: 50%;
   transform: translateX(-50%) translateY(-50%);
-  background-image: radial-gradient(circle at center,
-      transparent,
-      #000000bf 10%);
+  background-image: radial-gradient(
+    circle at center,
+    transparent,
+    #000000bf 10%
+  );
 }
 </style>
 
