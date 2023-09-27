d0da5068b6e21de1b4c30ecb237db04f163c326b
a0956154555 <wayne870726@yahoo.com.tw>
2023-09-27 19:28:48 +0800

    改良拉霸頁按鈕樣式 title hover顏色

--- src/views/InteractView.vue
+++ src/views/InteractView.vue
@@ -5,26 +5,30 @@
         @click="showQuiz"
         :quiz1="quizz"
         class="titleChangeBtn"
+        @mouseenter="onMouseEnter(1)"
+        @mouseleave="onMouseLeave(1)"
+        :class="{ changeHoverColor: isHover }"
       ></interactBtn>
       <interactBtn
         @click="showLaba"
         :laba1="labaa"
         class="titleChangeBtn"
+        @mouseenter="onMouseEnter(2)"
+        @mouseleave="onMouseLeave(2)"
+        :class="{ changeHoverColor: isHover2 }"
       ></interactBtn>
       <!-- <span >問卷</span>
       <span >拉霸</span> -->
     </div>
     <div class="mainpart">
-    <quiz2 v-if="isQuiz"></quiz2>
-    <laba v-if="isLaba"></laba>
+      <quiz2 v-if="isQuiz"></quiz2>
+      <laba v-if="isLaba"></laba>
     </div>
     <!-- <button @click="this.$route.path('../')"></button> -->
     <div class="deco">
-         <memAreaBG></memAreaBG>
+      <memAreaBG></memAreaBG>
     </div>
-
   </div>
- 
 </template>
 <script>
 import quiz2 from "../components/quiz2.vue";
@@ -36,6 +40,9 @@ export default {
   name: "product",
   data() {
     return {
+      alreadyHoverColor: "#ccc",
+      isHover: true,
+      isHover2: false,
       isQuiz: true,
       isLaba: false,
       quizz: "問卷",
@@ -52,30 +59,47 @@ export default {
   computed: {},
   watch: {},
   methods: {
+    onMouseLeave(idx) {
+      if (idx == 1 && !this.isQuiz) {
+        this.isHover = false;
+      } else if (idx == 2 && !this.isLaba) {
+        this.isHover2 = false;
+      }
+    },
+    onMouseEnter(idx) {
+      if (idx == 1) {
+        this.isHover = true;
+      } else {
+        this.isHover2 = true;
+      }
+    },
     showQuiz() {
       this.isQuiz = true;
       this.isLaba = false;
       this.$store.state.questionPic = true;
+
+      this.isHover2 = false;
     },
     showLaba() {
       this.isQuiz = false;
       this.isLaba = true;
+      this.isHover = false;
+      this.isHover2 = true;
     },
   },
   mounted() {},
 };
 </script>
 <style lang="scss" scoped>
-*{
-  background-color: #1D3D5B;
- 
+* {
+  background-color: #1d3d5b;
 }
 .interact {
   position: relative;
   .titleAll {
     z-index: 50;
     // border: 1px red solid;
-    background-color: #1D3D5B;
+    background-color: #1d3d5b;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -87,14 +111,17 @@ export default {
     .titleChangeBtn {
       margin: 15px;
     }
+    .changeHoverColor {
+      background-color: #1fb0cb;
+    }
   }
-  .mainpart{
+  .mainpart {
     z-index: 100;
   }
-  .deco{
-    background-color:  #1D3D5B;
+  .deco {
+    background-color: #1d3d5b;
     z-index: 0;
-    width:100%;
+    width: 100%;
     position: absolute;
     top: 70%;
     left: 0;
--- src/views/backEnd/orderMg.vue
+++ src/views/backEnd/orderMg.vue
@@ -9,15 +9,117 @@
         <input type="text" placeholder="請輸入訂單編號" />
         <div>搜尋</div>
       </div>
+      <div class="orderTableAll">
+        <div class="tableTitleAll">
+          <div>訂單編號</div>
+          <div>會員帳號</div>
+          <div>商品名稱</div>
+          <div>數量</div>
+          <div>時間</div>
+          <div>狀態</div>
+        </div>
+        <div v-for="i in prodOrder" class="singleOrderTable">
+          <div>{{ i.orderNumber }}</div>
+          <div></div>
+          <div>{{ i.memberAccount }}</div>
+          <div>
+            <div v-for="j in i.prodName">{{ j }}</div>
+          </div>
+
+          <div>{{ i.prodCount }}</div>
+          <div>{{ i.orderTime }}</div>
+          <div>{{ i.orderState }}</div>
+          <button>修改</button>
+        </div>
+      </div>
     </TabPane>
-    <TabPane label="購票訂單" name="name2">标签二的内容</TabPane>
+    <TabPane label="購票訂單" name="name2">
+      <div class="titleAll">
+        <select name="" id="">
+          <option value="">訂單編號</option>
+          <option value="">會員姓名</option>
+        </select>
+        <input type="text" placeholder="請輸入訂單編號" />
+        <div>搜尋</div>
+      </div>
+      <div class="ticketTableAll">
+        <div class="ticketTableTitleAll">
+          <div>訂單編號</div>
+          <div>會員帳號</div>
+          <div>票種</div>
+          <div>數量</div>
+          <div>票卷日期</div>
+        </div>
+        <div v-for="i in ticketOrder" class="singleTicketBar">
+          <div>{{ i.orderNumber }}</div>
+          <div>{{ i.memberAccount }}</div>
+          <div>
+            <div v-for="j in i.ticketType">{{ j }}</div>
+          </div>
+          <div>
+            <div v-for="j in i.ticketCount">{{ j }}</div>
+          </div>
+          <div>{{ i.ticketDate }}</div>
+        </div>
+      </div>
+    </TabPane>
   </Tabs>
 </template>
 <script>
 export default {
   name: "orderMg",
   data() {
-    return {};
+    return {
+      prodOrder: [
+        {
+          orderNumber: "abc12345",
+          memberAccount: "asv3567878",
+          prodName: ["海豚抱枕"],
+          prodCount: 1,
+          orderTime: "2023.08.31",
+          orderState: "問題訂單",
+        },
+        {
+          orderNumber: "abc12345",
+          memberAccount: "asv3567878",
+          prodName: ["海豚抱枕"],
+          prodCount: 1,
+          orderTime: "2023.08.31",
+          orderState: "確認訂單",
+        },
+        {
+          orderNumber: "abc12345",
+          memberAccount: "asv3567878",
+          prodName: ["海豚抱枕", "海星吊飾", "海星吊飾"],
+          prodCount: 1,
+          orderTime: "2023.08.31",
+          orderState: "問題訂單",
+        },
+      ],
+      ticketOrder: [
+        {
+          orderNumber: "a123456",
+          memberAccount: "asv3567878",
+          ticketType: ["成人票"],
+          ticketCount: [1],
+          ticketDate: "2023.08.31",
+        },
+        {
+          orderNumber: "a123456",
+          memberAccount: "asv3567878",
+          ticketType: ["學生票", "兒童票", "成人票"],
+          ticketCount: [2, 1, 3],
+          ticketDate: "2023.08.31",
+        },
+        {
+          orderNumber: "a123456",
+          memberAccount: "asv3567878",
+          ticketType: ["成人票"],
+          ticketCount: [1],
+          ticketDate: "2023.08.31",
+        },
+      ],
+    };
   },
   computed: {},
   mounted() {},
@@ -28,5 +130,59 @@ export default {
 <style lang="scss" scoped>
 .titleAll {
   display: flex;
+  select {
+    margin-right: 15px;
+  }
+  input {
+    margin-right: 15px;
+  }
+  div {
+    border: 1px black solid;
+    padding: 3px 10px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+}
+.ticketTableAll {
+  .ticketTableTitleAll {
+    padding: 10px 0px;
+    border-bottom: 1px black solid;
+    display: flex;
+    justify-content: space-around;
+    div {
+      flex-grow: 1;
+      &:last-child {
+        flex-grow: 3;
+      }
+    }
+  }
+  .singleTicketBar {
+    display: flex;
+    border-bottom: 1px black solid;
+    padding-bottom: 10px;
+    align-items: center;
+  }
+}
+.orderTableAll {
+  .tableTitleAll {
+    display: flex;
+    justify-content: space-around;
+    padding-bottom: 10px;
+    border-bottom: 1px black solid;
+    padding-top: 10px;
+    div {
+      flex-grow: 1;
+      &:last-child {
+        flex-grow: 1.5;
+      }
+    }
+  }
+  .singleOrderTable {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px 10px 10px 0px;
+    border-bottom: 1px black solid;
+  }
 }
 </style>
