dbedd3819385ebfbcacfe3ec946050f4a565d9f1
jason <abc@gmail.com>
2023-10-16 09:59:23 +0800

    會員頭像 尚無法塞入資料庫

--- public/php/memAvatar.php
+++ public/php/memAvatar.php
@@ -4,33 +4,20 @@ header("Content-Type: application/json");
 
 require_once("connect.php");
 
-// get
-try {
-
-    $sql = "SELECT * FROM member";
-    $stmt = $pdo->query($sql);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    echo json_encode($result);
-} catch (PDOException $e) {
-    $response = array('error' => true, 'msg' => '获取管理员数据失败: ' . $e->getMessage());
-    echo json_encode($response);
-}
-
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $uploadedFile = $_FILES["image"]["name"];
     $mem_id = $_POST["mem_id"];
 
     // 檢查文件是否成功上傳
-    if ($_FILES["image"]["error"] !== UPLOAD_ERR_OK) {
-        $response = [
-            "error" => true,
-            "message" => "文件上傳失敗。"
-        ];
-        echo json_encode($response);
-        exit;
-    }
+    // if ($_FILES["image"]["error"] !== UPLOAD_ERR_OK) {
+    //     $response = [
+    //         "error" => true,
+    //         "message" => "文件上傳失敗。"
+    //     ];
+    //     echo json_encode($response);
+    //     exit;
+    // }
 
     $fileName = uniqid();
     $fileExt = pathinfo($uploadedFile, PATHINFO_EXTENSION);
@@ -44,33 +31,41 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $targetFile = $fileName . "." . $fileExt;
 
     if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetDirectory . $targetFile)) {
-        // 文件上傳成功，接下來更新數據庫
-        $update_sql = "UPDATE member SET mem_pic = :mem_pic WHERE mem_id = :mem_id";
-        $update_stmt = $pdo->prepare($update_sql);
-        $update_stmt->bindValue(":mem_id", $mem_id, PDO::PARAM_INT);
-        $update_stmt->bindValue(":mem_pic", $targetFile);
-
-        if ($update_stmt->execute()) {
-            $response = [
-                "success" => true,
-                "message" => "更新成功。",
-            ];
-        } else {
-            $errorInfo = $update_stmt->errorInfo();
-            $response = [
-                "error" => true,
-                "message" => "更新失敗：" . $errorInfo[2],
-            ];
-        }
+        $response = [
+            "success" => true,
+            "message" => "文件上傳成功。",
+            "file_path" => $targetFile,
+            "fileName" => $targetFile
+        ];
+        echo json_encode($response);
     } else {
         $response = [
             "error" => true,
-            "message" => "文件複製失敗。"
+            "message" => "文件上傳失敗。"
+        ];
+        echo json_encode($_FILES["image"]["tmp_name"]);
+    }
+    // 文件上傳成功，接下來更新數據庫
+    $update_sql = "INSERT INTO member(mem_id, mem_pic) VALUES (:mem_id, :mem_pic)";
+    $update_stmt = $pdo->prepare($update_sql);
+    $update_stmt->bindValue(":mem_id", $mem_id, PDO::PARAM_INT);
+    $update_stmt->bindValue(":mem_pic", $targetFile);
+
+    if ($update_stmt->execute()) {
+        $response = [
+            "success" => true,
+            "message" => "更新成功。",
+        ];
+    } else {
+        $errorInfo = $update_stmt->errorInfo();
+        $response = [
+            "error" => true,
+            "message" => "更新失敗：" . $errorInfo[2],
         ];
     }
-
-    echo json_encode($response);
 }
+
+    // echo json_encode($response);
 
 // 用於數據庫操作的其餘代碼，取消註釋並正確處理
 
@@ -122,4 +117,3 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 //         $message = '<b>Could not upload file : </b>' . $error;
 //     }
 // }
-?> 
